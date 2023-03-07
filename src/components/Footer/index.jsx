0165b4f887ffd510766e75ab03a8ec6a8e189726
Max Vinicius <app.maxvinicius@gmail.com>
2023-03-06 22:15:41 -0300

    feat: add footer

---
+++ src/components/Footer/index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Footer = () => {
+	return (
+		<>
+			<div className='text-center font-bold'>
+				<h4>
+					© {new Date().getFullYear()} Max Vinícius.
+				</h4>
+			</div>
+		</>
+	)
+}
+
+export default Footer
+
---
+++ src/components/Footer/index.test.jsx
@@ -0,0 +1,8 @@
+import { render } from '@testing-library/react'
+import Footer from '.'
+
+describe('Footer', () => {
+  it('renders component Footer', () => {
+    render(<Footer />)
+  })
+})
